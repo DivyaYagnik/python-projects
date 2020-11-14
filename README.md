4899aa7dc190511a66b23217f3020107be7ab4fa
DivTatoDaBau5 <divya.yagnik@gmail.com>
2020-11-14 18:05:06 +1100

    Added stock web scraper

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # List of python projects
 
 - Weather app using OpenWeather API
+- Live web scraper that can get the price of any stock on Yahoo Finance (using BeautifulSoup package)
---
+++ stock_web_scraper.py
@@ -0,0 +1,32 @@
+from bs4 import BeautifulSoup
+import requests 
+import time
+
+def priceTracker(): 
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    price = soup.find_all('div', {'class':'D(ib) Va(m) Maw(65%) Ov(h)'})[0].find('span').text
+
+    print(stock + ' price: ' + price)
+
+
+if __name__ == '__main__':
+    choice = input("Live tracker (l) or just display current price (c)? ")
+
+    stock_url = "https://au.finance.yahoo.com/quote/{}?p={}"
+
+    stock = input("Enter stock name (e.g. TSLA): ")
+
+    response = requests.get(stock_url.format(stock, stock))
+
+    if choice == 'l':
+        print("Press ctrl+c to stop program")
+        while True:
+            priceTracker()
+            time.sleep(1)
+            
+    elif choice == 'c':
+        priceTracker()
+    else:
+        print("Invalid input")
